b6cd80a68ce27cf67cc994472d8abf99f1c218a7
Michael Pallister <iam@michaelpallister.dev>
2022-06-01 12:22:27 +0100

    Removing optimisation

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,8 +18,6 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
           <link
             href='https://fonts.googleapis.com/css2?family=Raleway:wght@400;900&display=swap'
             rel='stylesheet'
