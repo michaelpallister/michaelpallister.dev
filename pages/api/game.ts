b360e860987d096ba964bbeb701b60e6d3f22ff7
Michael Pallister <iam@michaelpallister.dev>
2022-06-07 08:36:47 +0100

    Updating the script to push the content to the db

--- pages/api/game.ts
+++ pages/api/game.ts
@@ -1,42 +1,44 @@
-const cheerio = require('cheerio');
+import * as cheerio from 'cheerio';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/prisma';
 
-const games = async (req: any, res: any) => {
-  if (req.method === 'GET') {
-    try {
-      const response = await fetch(`https://psnprofiles.com/micpuk`);
-      const htmlString = await response.text();
-      const $ = cheerio.load(htmlString);
-      const games: Object[] = [];
-      $('#gamesTable tr:nth-of-type(-n + 10)').each(function (
-        this: HTMLElement
-      ) {
-        const title = $(this).find('.title').text();
-        const completion = $(this).find('.progress-bar span').text();
-        const platform = $(this).find('.platforms .platform').text();
-        const platinum = $(this).find('.platinum').hasClass('earned');
-        const image = $(this).find('.game img').attr('src');
+const games = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const response = await fetch(`https://psnprofiles.com/micpuk`);
+    const htmlString = await response.text();
+    const $ = cheerio.load(htmlString);
+    const games: Object[] = [];
+    $('#gamesTable tr:nth-of-type(-n + 9)').each(function (this) {
+      const title = $(this).find('.title').text();
+      const completion = $(this).find('.progress-bar span').text();
+      const platform = $(this).find('.platforms .platform').text();
+      const platinum = $(this).find('.platinum').hasClass('earned');
+      const image = $(this).find('.game img').attr('src');
 
-        const obj = {
-          title,
-          completion,
-          platform,
-          platinum,
-          image,
-        };
+      const obj = {
+        title,
+        completion,
+        platform,
+        platinum,
+        image,
+      };
 
-        games.push(obj);
-      });
+      games.push(obj);
+    });
 
-      res.statusCode = 200;
-      return res.json({
-        games,
-      });
-    } catch (e) {
-      res.statusCode = 404;
-      return res.json({
-        error: `Unable to find games`,
-      });
-    }
+    await prisma.games.createMany({
+      data: games,
+    });
+
+    res.statusCode = 200;
+    return res.json({
+      games,
+    });
+  } catch (e) {
+    res.statusCode = 404;
+    return res.json({
+      error: `Unable to find games`,
+    });
   }
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
+import prisma from '../lib/prisma';
+import Image from 'next/image';
 
-const Home: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const allGames = await prisma.games.findMany();
+  return { props: { allGames } };
+};
+
+const Home: NextPage = (props) => {
+  const games = props.allGames.map((g) => (
+    <li key={g.id} className='bg-jet rounded p-4 bg-opacity-25'>
+      <Image src={g.image} height='56' width='100' alt='' objectFit='cover' />
+      <p>{g.title}</p>
+      <p>{g.completion}</p>
+      <p>{g.platform}</p>
+      <p>{g.platinum ? 'true' : 'false'}</p>
+    </li>
+  ));
+
   return (
-    <div className='flex flex-col justify-between h-screen'>
+    <div className='flex flex-col justify-between'>
       <Head>
         <title>
           Michael Pallister | Frontend Developer | michaelpallister.dev
@@ -17,7 +34,7 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <header className='container h-16 my-3'>
+      <header className='container h-16 mt-3 mb-20'>
         <Link href='/'>
           <a className='inline-block'>
             <svg
@@ -38,10 +55,10 @@ const Home: NextPage = () => {
         </Link>
       </header>
 
-      <main className='container h-100'>
-        <p className='text-6xl lg:text-8xl font-bold flex items-center'>
+      <main className='container'>
+        <h1 className='text-6xl lg:text-8xl font-bold flex items-center'>
           Hi, I am Michael.
-        </p>
+        </h1>
         <p className='text-2xl mt-2 lg:text-4xl lg:mt-4'>
           I build websites, collect games consoles and{' '}
           <a
@@ -54,6 +71,8 @@ const Home: NextPage = () => {
           </a>
           .
         </p>
+        <h2 className='mt-20 mb-4 text-xl font-bold'>Recently played</h2>
+        <ul className='mb-10 grid grid-cols-3 gap-4'>{games}</ul>
       </main>
       <footer className='container h-16 flex items-center'>
         <p className='text-sm'>&copy; Michael Pallister</p>
