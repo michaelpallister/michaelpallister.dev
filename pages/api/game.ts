0467d251f198f59422e70b3cd7aba47a63268834
Michael Pallister <michael.pallister@team.bumble.com>
2024-10-10 11:38:52 +0100

    Updating CSS selector to only pick the first option which should always be the console

--- pages/api/game.ts
+++ pages/api/game.ts
@@ -1,6 +1,6 @@
-import * as cheerio from 'cheerio';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { getXataClient } from '../../lib/xata';
+import * as cheerio from "cheerio";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getXataClient } from "../../lib/xata";
 
 interface Games {
   title: string;
@@ -12,7 +12,7 @@ interface Games {
 
 const games = async (req: NextApiRequest, res: NextApiResponse) => {
   const xata = getXataClient();
-  const records = await xata.db.Games.select(['id']).getAll();
+  const records = await xata.db.Games.select(["id"]).getAll();
 
   if (records) {
     records.forEach(function (record) {
@@ -24,23 +24,25 @@ const games = async (req: NextApiRequest, res: NextApiResponse) => {
     const htmlString = await response.text();
     const $ = cheerio.load(htmlString);
     const games: Games[] = [];
-    $('#gamesTable tr:nth-of-type(-n + 12)').each(function (this) {
-      const title = $(this).find('.title').text();
-      const completion = $(this).find('.progress-bar span').text();
-      const platform = $(this).find('.platforms .platform').text();
-      const platinum = $(this).find('.platinum').hasClass('earned');
-      const image = $(this).find('.game img').attr('src');
+    $("#gamesTable tr:nth-of-type(-n + 12)").each(function (this) {
+      const title = $(this).find(".title").text();
+      const completion = $(this).find(".progress-bar span").text();
+      const platform = $(this)
+        .find(".platforms .platform:first-of-type")
+        .text();
+      const platinum = $(this).find(".platinum").hasClass("earned");
+      const image = $(this).find(".game img").attr("src");
       const stringDate = $(this)
-        .find('.small-info:nth-of-type(3)')
+        .find(".small-info:nth-of-type(3)")
         .text()
         .trim()
-        .split('\n')[0];
+        .split("\n")[0];
 
-      const cleanDateStr = stringDate.replace(/(\d+)(st|nd|rd|th)/, '$1');
+      const cleanDateStr = stringDate.replace(/(\d+)(st|nd|rd|th)/, "$1");
       const parsedDate = new Date(cleanDateStr);
       const year = parsedDate.getFullYear();
-      const month = String(parsedDate.getMonth() + 1).padStart(2, '0');
-      const day = String(parsedDate.getDate()).padStart(2, '0');
+      const month = String(parsedDate.getMonth() + 1).padStart(2, "0");
+      const day = String(parsedDate.getDate()).padStart(2, "0");
       const date = `${year}-${month}-${day}`;
 
       const obj = {
